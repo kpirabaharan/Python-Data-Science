dd902179e10bef757ea82bb32f1ac0385e65df21
kpirabaharan <spkeeshi@gmail.com>
2022-07-29 20:15:49 -0400

    Init Commit

---
+++ numpy/numpyArrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+my_list = [1, 2, 3]
+
+# print(np.array(my_list))
+# print(np.arange(0, 10))
+# print(np.linspace(0, 5, 101))
+
+arr = np.arange(25)
+print(arr)
+
+arr = arr.reshape(5, 5)
+print(arr)
